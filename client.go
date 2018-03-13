a693cfc8e0bd8ffbca00b35abc9e4ce449ae401c
Amr Fathy <amruafathy@gmail.com>
2018-03-13 14:53:14 +0200

    Improve waiting && add number of client accesses

--- client.go
+++ client.go
@@ -8,12 +8,17 @@ import (
 	"encoding/binary"
 	"strconv"
 	"math/rand"
+	"sync"
+	"os"
+	"log"
 )
 
 var (
 	sAddr       string
 	cNumReaders int
 	cNumWriters int
+	cNumAccess  int
+	wg          sync.WaitGroup
 )
 
 func init() {
@@ -28,12 +33,15 @@ func init() {
 	cInfo, _ := cfg.GetSection("client")
 	cReaders, _ := cInfo.GetKey("num_readers")
 	cWriters, _ := cInfo.GetKey("num_writers")
+	cAccess, _ := cInfo.GetKey("num_accesses")
 
 	cReadersInt, _ := strconv.Atoi(cReaders.String())
 	cWritersInt, _ := strconv.Atoi(cWriters.String())
+	cAccessInt, _ := strconv.Atoi(cAccess.String())
 
 	cNumReaders = cReadersInt
 	cNumWriters = cWritersInt
+	cNumAccess = cAccessInt
 }
 
 func main() {
@@ -44,24 +52,29 @@ func main() {
 	defer conn.Close()
 
 	for i := 0; i < cNumReaders; i++ {
+		wg.Add(1)
 		go readerClient(i)
 	}
 
 	for i := 0; i < cNumWriters; i++ {
+		wg.Add(1)
 		go writerClient(i)
 	}
 
-	for {
-	}
+	wg.Wait()
 }
 
 func readerClient(idx int) {
+	defer wg.Done()
 	conn, _ := net.Dial("tcp", sAddr)
 	defer conn.Close()
 
+	logFile, _ := os.Create("./logs/readers/reader" + strconv.Itoa(idx) + ".log")
+	logger := log.New(logFile, "", log.Ltime)
+
 	var number int64
 
-	for {
+	for i := 0; i < cNumAccess; i++ {
 		// request
 		conn.Write([]byte("read\n"))
 
@@ -69,20 +82,28 @@ func readerClient(idx int) {
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		fmt.Println("Reader #", idx, "=> value from server", number)
+		logger.Printf("%d\t%d\n", idx, number)
 	}
 }
 
 func writerClient(idx int) {
+	defer wg.Done()
 	conn, _ := net.Dial("tcp", sAddr)
 	defer conn.Close()
 
-	for {
+	logFile, _ := os.Create("./logs/writers/writer" + strconv.Itoa(idx) + ".log")
+	logger := log.New(logFile, "", log.Ltime)
+
+	for i := 0; i < cNumAccess; i++ {
 		numToWrite := rand.Intn(100)
 		conn.Write([]byte("write " + strconv.Itoa(numToWrite) + "\n"))
 
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 		fmt.Println("Writer #", idx, "writing", numToWrite)
 		fmt.Println("Message from server", message)
+
+		logger.Printf("%d\t%d\n", idx, numToWrite)
 	}
 }
